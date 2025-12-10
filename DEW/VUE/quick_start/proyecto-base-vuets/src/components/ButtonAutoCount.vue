301657db85258155b218439970e6ea547ba4625c
edumel20 <rabadanmelianeduardo@gmail.com>
2025-12-10 17:15:19 +0000

    Se sigue con Vue

---
+++ DEW/VUE/quick_start/proyecto-base-vuets/src/components/ButtonAutoCount.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p>Soy ButtonAutoCount</p>
+        <button @click="count++">Pulsado {{ count }} veces.</button>
+        <br>
+        <input @onChange="a"></input>
+        <br>
+        <input @onChange="b"></input>
+
+        <p>Valor de a: {{ a }}</p>
+        <p>Valor de b: {{ b }}</p>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+    import {ref} from 'vue'
+    let count = ref<number>(0)
+    let a = ref<number>(0)
+    let b = ref<number>(0)
+</script>
+
+<style scoped>
+</style>
