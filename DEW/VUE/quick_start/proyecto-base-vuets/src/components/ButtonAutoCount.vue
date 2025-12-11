1ac5c6c3cb1a5698fc2135ea02bccd8d5660548b
edumel20 <rabadanmelianeduardo@gmail.com>
2025-12-11 16:04:49 +0000

    se termina el ejemplo de Vue de clase

--- DEW/VUE/quick_start/proyecto-base-vuets/src/components/ButtonAutoCount.vue
+++ DEW/VUE/quick_start/proyecto-base-vuets/src/components/ButtonAutoCount.vue
@@ -1,23 +1,44 @@
 <template>
-    <div>
-        <p>Soy ButtonAutoCount</p>
-        <button @click="count++">Pulsado {{ count }} veces.</button>
+    <div class="flex flex-column" style="width: 50%; text-align: center;">
+        <h3>Soy ButtonAutoCount</h3>
+        <Button @click="count++">Pulsado {{ count }} veces.</Button>
         <br>
-        <input @onChange="a"></input>
         <br>
-        <input @onChange="b"></input>
+        <InputNumber v-model.number="a"></InputNumber>
+        <br>
+        <InputNumber v-model.number="b"></InputNumber>
 
-        <p>Valor de a: {{ a }}</p>
-        <p>Valor de b: {{ b }}</p>
+        <p>El resultado de sumar {{ a }} y {{ b }} es {{ result }}</p>
+        
+        <h3>Suma usando objetos</h3>
+        <InputNumber v-model.number="suma.a"></InputNumber>
+        <br>
+        <InputNumber v-model.number="suma.b"></InputNumber>
+        <p>El resultado de sumar {{ suma.a }} y {{ suma.b }} es {{ suma.result }}</p>
 
     </div>
 </template>
 
 <script setup lang="ts">
-    import {ref} from 'vue'
-    let count = ref<number>(0)
-    let a = ref<number>(0)
-    let b = ref<number>(0)
+import Button from 'primevue/button'
+import InputNumber from 'primevue/inputnumber'
+import { computed, ref, reactive} from 'vue'
+    
+function sumar() {
+    return a.value + b.value
+}
+
+const count = ref<number>(0)
+const a = ref<number>(0)
+let b = ref<number>(0)
+const result = computed<number>(sumar)
+const suma = reactive ({
+    a:0,
+    b:0,
+    get result (){
+        return this.a + this.b
+    }
+})
 </script>
 
 <style scoped>
--- DEW/VUE/quick_start/proyecto-base-vuets/src/main.ts
+++ DEW/VUE/quick_start/proyecto-base-vuets/src/main.ts
@@ -1,4 +1,7 @@
 import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+import '/node_modules/primeflex/primeflex.css'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
@@ -6,6 +9,11 @@ import router from './router'
 
 const app = createApp(App)
 
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+})
 app.use(createPinia())
 app.use(router)
 
