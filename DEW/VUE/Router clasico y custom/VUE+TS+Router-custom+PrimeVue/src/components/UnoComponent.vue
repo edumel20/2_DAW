76aed342cf4e636782dfe2181295b894c021270d
Eduardo Rabadan Melian <rabadanmelianeduardo@gmail.com>
2026-01-12 20:19:17 +0000

    se revisa VUE

---
+++ DEW/VUE/Router clasico y custom/VUE+TS+Router-custom+PrimeVue/src/components/UnoComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Uno Componente</h1>
+        <p>Contador actual: {{ counter.count }}</p>
+        <p>Doble del contador: {{ counter.doubleCount }}</p>
+        <button @click="counter.increment">Incrementar</button>
+        <DosComponent />
+    </div>
+</template>
+
+<script setup lang="ts">
+    import DosComponent from "./Doscomponent.vue"
+    import { useCounterStore } from "@/stores/counter";
+
+    const counter = useCounterStore()
+</script>
+
